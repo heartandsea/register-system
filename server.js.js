7be88a9d48729af39925a186f46e4f04f079ab2f
heartandsea <117577961+heartandsea@users.noreply.github.com>
2026-04-08 10:55:37 +0800

    Update server.js.js

--- server.js.js
+++ server.js.js
@@ -17,7 +17,7 @@ const TARGET_EMAIL = '1226505228@qq.com';   // 接收注册信息的邮箱（固
 const transporter = nodemailer.createTransport({
     host: 'smtp.qq.com',
     port: 587,
-    secure: true,
+    secure: false,
     auth: {
         user: EMAIL_USER,
         pass: EMAIL_PASS
